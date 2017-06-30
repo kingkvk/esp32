78c2c1d14f664ad286847432d5a39ef517ea0be5
karthick <kingkvk@users.noreply.github.com>
2017-06-30 19:47:43 +0530

    Add files via upload

---
+++ ESP Load Cell.ino
@@ -0,0 +1,42 @@
+
+/*
+   HX711 ADC with ESP32
+   Author:KINGKVK
+   Project:Reading ADC Value 
+   
+
+
+*/
+#include "HX711.h"
+
+void adc_read();
+unsigned long count = 0;
+HX711 scale_1(5, 4); // data,clk
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+}
+
+char sbuffer[6] = {0};
+
+void loop()
+{
+
+  adc_read();
+ 
+}
+
+void adc_read()
+{
+  count  = scale_1.read1() >> 10;
+  delay(100);
+  sprintf(sbuffer, "%06ld", count);
+  Serial.write('[');
+  Serial.write(sbuffer);
+  Serial.write(']');
+  Serial.write(0x0d);
+  Serial.write(0x0a);
+}
